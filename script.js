0425b5dc0c5605bc252b3411a92fbc3d0fd70709
Witold <blue3656@gmail.com>
2017-01-29 04:26:56 +0000

    sorted api

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 $(document).ready(function(){
 
-	$.getJSON("https://api.twitch.tv/kraken/streams?callback=?", function(data){
+	$.ajax({
+		type: 'GET',
+		url: "https://api.twitch.tv/kraken/streams/featured",
+		//url: "https://api.twitch.tv/kraken/streams?game=dota",
+		headers: {
+			'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
+		},
+		success: function(data) {
+			console.log(data);
+			var channeLink = {};
+			var channeLink = data['featured'][0]['stream']['channel']['url'];
+			var status = data['featured'][0]['stream']['channel']['status'];
+			var logo = data['featured'][0]['image'];
+			//var link = JSON.parse(JSON.stringify(channeLink));
+			//var link = JSON.stringify(channeLink);
+			console.log(channeLink);
+			console.log(logo);
+			console.log(status);
 
-		console.log(data);
-
-
+		}
 	});
-	
+
+	$.ajax({
+		type: 'GET',
+		url: "https://api.twitch.tv/kraken/streams/ddrjake",
+		headers: {
+			'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
+		},
+		success: function(data) {
+			if(data['stream'] === null){
+				console.log('ddrjake is offline');
+			}
+		}
+
+	});		
 });
