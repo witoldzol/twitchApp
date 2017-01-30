160518122167a6e4210ce630f36667bd8249257a
Witold <blue3656@gmail.com>
2017-01-30 02:09:37 +0000

    built javascript foundation

--- script.js
+++ script.js
@@ -9,30 +9,27 @@ $(document).ready(function(){
 		},
 		success: function(data) {
 			console.log(data);
-			var channeLink = {};
+			//array variable, we have to break the path to final objects in order to loop them
+			var arr = data['featured'];
+			var channel = ['stream']['channel'];
+
+			var channelName = data['featured'][0]['stream']['channel']['display_name'];
 			var channeLink = data['featured'][0]['stream']['channel']['url'];
 			var status = data['featured'][0]['stream']['channel']['status'];
-			var logo = data['featured'][0]['image'];
+			//var image = data['featured'][0]['image'];
+			var logo = data['featured'][0]['stream']['channel']['logo']
 			//var link = JSON.parse(JSON.stringify(channeLink));
 			//var link = JSON.stringify(channeLink);
 			console.log(channeLink);
 			console.log(logo);
 			console.log(status);
+			for(i=0; i<data['featured'].length; i++){
 
-		}
-	});
-
-	$.ajax({
-		type: 'GET',
-		url: "https://api.twitch.tv/kraken/streams/ddrjake",
-		headers: {
-			'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
-		},
-		success: function(data) {
-			if(data['stream'] === null){
-				console.log('ddrjake is offline');
+			$("#streamContainer").append($('<div id="stream"><img src="'+arr[i]+'["stream"]["channel"]"><p>'+channelName+'</p><p>'+status+'</p></div>'));
+			//$("#streamContainer").append($('<div id="stream"><img src="'+logo+'"><p>'+channelName+'</p><p>'+status+'</p></div>'));
 			}
 		}
+
 
 	});		
 });
