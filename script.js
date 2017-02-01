af5a15ba087e2929b5ffdadd8bd79383e6c4d65f
Witold <blue3656@gmail.com>
2017-02-01 21:40:33 +0000

    error function added

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ $(document).ready(function(){
 	$.ajax({
 		type: 'GET',
 		url: "https://api.twitch.tv/kraken/channels/freecodecamp",
+		//url: "https://api.twitch.tv/kraken/channels/brunofin",
 		headers: {
 			'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
+		},
+		error: function(){
+			$('#streamContainer').append($('<div id="stream" class="stream0"><p id="notExist">Stream doesnt exist</p><div>'));
 		},
 		success: function(data) {
 			console.log(data);
@@ -12,6 +16,7 @@ $(document).ready(function(){
 			var status = data['status'];
 			var logo = data['logo']
 			var channelName = data['display_name'];
+			$("#streamContainer").append($('<a href='+channeLink+' target="_blank" <div id="stream" class="stream0"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
 			function onOff(){
 				$.ajax({
 					type: 'GET',
@@ -20,25 +25,17 @@ $(document).ready(function(){
 						'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
 					},
 					success: function(data) {
+
 						if (data['stream'] == null){
-							$(".stream0").append($('<p>Offline</p>'));
+							$("#status").append($('<p id="offline">Offline</p>'));
 						}else {
-							$(".stream0").append($('<p>Online</p>'));
+							$("#status").append($('<p id="online">Online</p>'));
 						}
-					}
-
-
+					},
 				});	
-
-
 			}
-
-			$("#streamContainer").append($('<a href='+channeLink+' target="_blank" <div id="stream" class="stream0"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
-			//$("#streamContainer").append($('<div id="stream"><img src="'+logo+'"><p>'+channelName+'</p><p>'+status+'</p></div>'));
 			onOff();
 		}
-
-
 	});	
 
 	
@@ -56,13 +53,14 @@ $(document).ready(function(){
 			console.log(channeLink);
 			console.log(logo);
 			console.log(data['featured'].length);
-			for(i=0; i<data['featured'].length; i++){
+			for(i=0; i<10; i++){
 
 			var channeLink = data['featured'][i]['stream']['channel']['url'];
 			var status = data['featured'][i]['stream']['channel']['status'];
 			var logo = data['featured'][i]['stream']['channel']['logo']
 			var channelName = data['featured'][i]['stream']['channel']['display_name'];
-			$("#streamContainer").append($('<a href='+channeLink+' target="_blank" <div id="stream" class="stream'+i+'"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
+			//creating stream containers and giving them a class number so i can modify them on the fly in future (i+1)
+			$("#streamContainer").append($('<a href='+channeLink+' target="_blank" <div id="stream" class="stream'+(i+1)+'"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
 			//$("#streamContainer").append($('<div id="stream"><img src="'+logo+'"><p>'+channelName+'</p><p>'+status+'</p></div>'));
 			}
 		}
