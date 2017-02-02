e8d4bfba8f6e1b5d16df70e68953f040d39c0ec2
Witold <blue3656@gmail.com>
2017-02-02 21:14:04 +0000

    added new ajax call

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+	//makes a call and searches streams api to see if channel is online
+	
+	function checkIfOnline(targetDiv){
+		var targetDiv = targetDiv;
+		$.ajax({
+			type: 'GET',
+			url: "https://api.twitch.tv/kraken/streams/freecodecamp",
+			headers: {
+				'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
+			},
+			success: function(data, targetDiv) {
+				if (data['stream'] == null){
+					console.log("Offline");
+					//$(targetDiv).append($('<p id="offline">Offline</p>'));
+				}else {
+					return "Online";
+					//$(targetDiv).append($('<p id="online">Online</p>'));
+				}
+			},
+		});	
+	}
+
 	$.ajax({
 		type: 'GET',
 		url: "https://api.twitch.tv/kraken/channels/freecodecamp",
@@ -7,34 +29,19 @@ $(document).ready(function(){
 			'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
 		},
 		error: function(){
-			$('#streamContainer').append($('<div id="stream" class="stream0"><p id="notExist">Stream doesnt exist</p><div>'));
+			//test set only for brunofin channel
+			$('#featured').append($('<div id="stream" class="stream0"><p>brunofin</p><p id="notExist">Stream doesnt exist</p><div>'));
 		},
 		success: function(data) {
 			console.log(data);
-
+			var targetDiv = "#featured"
 			var channeLink = data['url'];
 			var status = data['status'];
 			var logo = data['logo']
 			var channelName = data['display_name'];
-			$("#streamContainer").append($('<a href='+channeLink+' target="_blank" <div id="stream" class="stream0"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
-			function onOff(){
-				$.ajax({
-					type: 'GET',
-					url: "https://api.twitch.tv/kraken/streams/freecodecamp",
-					headers: {
-						'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
-					},
-					success: function(data) {
-
-						if (data['stream'] == null){
-							$("#status").append($('<p id="offline">Offline</p>'));
-						}else {
-							$("#status").append($('<p id="online">Online</p>'));
-						}
-					},
-				});	
-			}
-			onOff();
+			$("#featured").append($('<a href='+channeLink+' target="_blank" <div id="stream" class="stream0"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
+			
+			checkIfOnline();
 		}
 	});	
 
@@ -53,18 +60,43 @@ $(document).ready(function(){
 			console.log(channeLink);
 			console.log(logo);
 			console.log(data['featured'].length);
-			for(i=0; i<10; i++){
+			for(i=0; i<5; i++){
 
 			var channeLink = data['featured'][i]['stream']['channel']['url'];
 			var status = data['featured'][i]['stream']['channel']['status'];
 			var logo = data['featured'][i]['stream']['channel']['logo']
 			var channelName = data['featured'][i]['stream']['channel']['display_name'];
 			//creating stream containers and giving them a class number so i can modify them on the fly in future (i+1)
-			$("#streamContainer").append($('<a href='+channeLink+' target="_blank" <div id="stream" class="stream'+(i+1)+'"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
+			$("#featured").append($('<a href='+channeLink+' target="_blank" <div id="stream" class="stream'+(i+1)+'"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
 			//$("#streamContainer").append($('<div id="stream"><img src="'+logo+'"><p>'+channelName+'</p><p>'+status+'</p></div>'));
 			}
 		}
-
-
 	});		
+	//searchbox 
+	$("#search").on("keyup", function(){
+		
+		var search = $("#search").val();
+
+		$.ajax({
+			type: 'GET',
+			url: "https://api.twitch.tv/kraken/channels/"+search+"",
+			headers: {
+				'Client-ID': "4rbydljvsruh725vzyflpl3dpurhpa"
+			},
+			success: function(data) {
+				var targetDiv = "#result";
+				console.log(data);
+				var channeLink = data['url'];
+				var status = data['status'];
+				var logo = data['logo']
+				var channelName = data['display_name'];
+				$(targetDiv).append($('<a href='+channeLink+' target="_blank" <div id="stream"><img src="'+logo+'"><p id="name">'+channelName+'</p><p id="status">'+status+'</p></div></a>'));
+				
+				
+				$(targetDiv).append($('<p id="offline">'+ checkIfOnline() +'</p>'));
+
+			}
+
+		});
+	});
 });
